301f2d0c66ed5e234257904870f295a8b5c69a59
Tonny <39811427+hrdapr@users.noreply.github.com>
2019-02-04 00:13:49 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="Tonny";
+
+//user_gaid wajib isi
+$user_gaid= "6c93fbf5-9d1c-4123-a0f9-cff8b6d80ecb";
+//user_token wajib di isi //
+$user_token="AbwZp3O-0mnM6o5K";
+
+//user_id wajib di isi //
+$user_id="Facebook_1407219686081482";
+
+// invite_code wajib di isi //
+$invite_code="F6RatHl4pe";
+
+// Bearer wajib di isi //
+$Bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xNDA3MjE5Njg2MDgxNDgyIiwiZXhwIjoiMTU1MTgwMDc1NTY2MiJ9.M5Ec3KG-FRvUwNywrNjmJz0ReVstoyy_XdLNk0s1Vck";
+
+
+
+?>
